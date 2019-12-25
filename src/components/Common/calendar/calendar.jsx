28b67e3ee64913dcbfdf5884f7d4f387d05dd584
administrator <osekovs@mail.ru>
2019-12-25 18:52:50 +0300

    feat: calendar added

--- src/components/Common/calendar/calendar.jsx
+++ src/components/Common/calendar/calendar.jsx
@@ -9,8 +9,10 @@ class Wrapper extends React.Component {
     constructor(props){
         super(props);
         let now = new Date();
-        let start = moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0,0,0,0));
-        let end =  moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(),now.getMinutes(),0,0));
+        let start = moment(this.props.timeFilter.from);
+        let end =  moment(this.props.timeFilter.to);
+        // let start = moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0,0,0,0));
+        // let end =  moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(),now.getMinutes(),0,0));
         this.dateOptions = {
             year: 'numeric',
             month: 'numeric',
@@ -24,24 +26,39 @@ class Wrapper extends React.Component {
         }
  
         this.applyCallback = this.applyCallback.bind(this);
+        this.props.applyParentCallback = this.props.applyParentCallback.bind(this);
     }
  
     applyCallback(startDate, endDate){
+        // console.log(new Date(startDate._d))
+        // console.log(new Date(endDate._d))
         this.setState({
-                start: startDate,
-                end : endDate
-            }
-        )
+            start: startDate,
+            end : endDate
+        })
+        this.props.applyParentCallback(startDate, endDate);
     }
  
     render(){
+            // console.log(this.props)
+            // console.log(this.props.timeFilter.from)
+            // console.log(this.props.timeFilter.to)
             let now = new Date();
-            let start = moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0,0,0,0));
-            let end = moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(),now.getMinutes(),0,0));//moment(start).add(1, "days").subtract(2, "hour");
+            let start = this.props.timeFilter.from;
+            let end =  this.props.timeFilter.to;
+            // let start = moment(this.props.timeFilter.from);
+            // let end =  moment(this.props.timeFilter.to);
+            // let start = moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0,0,0,0));
+            // let end = moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(),now.getMinutes(),0,0));//moment(start).add(1, "days").subtract(2, "hour");
             let ranges = {
-                "Today Only": [moment(start), moment(end)],
-                "Yesterday Only": [moment(start).subtract(1, "days"), moment(end).subtract(1, "days")],
-                "3 Days": [moment(start).subtract(3, "days"), moment(end)]
+                "Сегодня": [moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0,0,0,0)), moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(),now.getMinutes(),0,0))],
+                "Вчера": [moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0,0,0,0)).subtract(1, "days"), moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23,59,0,0)).subtract(1, "days")],
+                "3 дня": [moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0,0,0,0)).subtract(3, "days"),  moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(),now.getMinutes(),0,0))],
+                "7 дней": [moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0,0,0,0)).subtract(7, "days"),  moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(),now.getMinutes(),0,0))],
+                "Месяц": [moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0,0,0,0)).subtract(1, "months"),  moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(),now.getMinutes(),0,0))],
+                "3 месяца": [moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0,0,0,0)).subtract(3, "months"),  moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(),now.getMinutes(),0,0))],
+                "Пол года": [moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0,0,0,0)).subtract(6, "months"),  moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(),now.getMinutes(),0,0))],
+                "Год": [moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0,0,0,0)).subtract(12, "months"),  moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(),now.getMinutes(),0,0))],
             }
             let local = {
                 "format":"DD.MM.YYYY, HH:mm",
@@ -67,23 +84,44 @@ class Wrapper extends React.Component {
                 maxDate: 'Максимальная дата',
                 close: 'Закрыть',
                 apply: 'Применить',
+              
                 cancel: 'Отменить'
             }
             let maxDate = moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(),now.getMinutes()+1,0,0));//moment(start).add(23, "hour")
+            let redTheme= 'rgb(236, 28, 36)'
+            let whiteTheme= 'rgb(255, 255, 255)'
+            let greyTheme = 'rgba(38, 49, 55, 0.5)'
             return(
                     <DateTimeRangeContainer 
                         ranges={ranges}
-                        start={this.state.start}
-                        end={this.state.end}
+                        // start={this.state.start}
+                        // end={this.state.end}
+                        start={start}
+                        end={end}
                         local={local}
                         maxDate={maxDate}
                         applyCallback={this.applyCallback}
+                       style={ {
+                            // fromDot: {backgroundColor: 'rgb(100, 0, 34)'},
+                            // toDot: {backgroundColor: 'rgb(0, 135, 255)'},
+                            fromDate: { backgroundColor: redTheme},
+                            toDate: {backgroundColor: redTheme},
+
+                            betweenDates: {backgroundColor: greyTheme},
+                            hoverCell: {backgroundColor: redTheme},
+                            customRangeButtons: {backgroundColor: whiteTheme},
+                            customRangeSelected: {backgroundColor: redTheme},
+                            standaloneLayout:{display:'flex', maxWidth:'fit-content'},
+                            hoverColourApply: {backgroundColor: redTheme},
+                        }}
                     >    
                         <FormControl
                         id="formControlsTextB"
                         type="text"
                         label="Text"
                         placeholder="Enter text"
+                        // value={'C ' +start.format('dd MM DD YY') + ' по '+end.format('dd MM DD YY')}
+                        // value={'C ' +new Date(start).toLocaleString("ru", this.dateOptions) + ' по '+new Date(end).toLocaleString("ru", this.dateOptions)}
                         value={'C ' +new Date(this.state.start).toLocaleString("ru", this.dateOptions) + ' по '+new Date(this.state.end).toLocaleString("ru", this.dateOptions)}
                         /> 
                     </DateTimeRangeContainer>
